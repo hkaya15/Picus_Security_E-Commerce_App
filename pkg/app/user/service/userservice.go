da7802acc802f7f3b7658cb61bb9d4387b0cc79c
hkaya15 <kayahuseyin15@gmail.com>
2022-04-09 02:44:58 +0300

    Add password hash

--- pkg/app/user/service/userservice.go
+++ pkg/app/user/service/userservice.go
@@ -8,6 +8,7 @@ import (
 	. "github.com/hkaya15/PicusSecurity/Final_Project/pkg/app/user/model"
 	. "github.com/hkaya15/PicusSecurity/Final_Project/pkg/app/user/repository"
 	. "github.com/hkaya15/PicusSecurity/Final_Project/pkg/base/errors"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
@@ -26,6 +27,11 @@ func (u *UserService) Save(user *User) (*User, error) {
 	if verifyEMail(user.Email) {
 		ver := verifyPassword(user.Password)
 		if ver == true {
+			hash,err:=hashPassword(user.Password)
+			if err!=nil{
+				return nil, NewRestError(http.StatusBadRequest,"Problem on creating password",nil)
+			}
+			user.Password=hash
 			return u.UserRepo.Save(user)
 		}
 		return nil, NewRestError(http.StatusBadRequest, "Password is weak! Please Enter password that contains 1 Number, 1 Upper, 1 Lower, 1 Special Char and 7 length", nil)
@@ -66,4 +72,9 @@ func verifyPassword(s string) bool {
 		}
 	}
 	return hasMinLen && hasUpper && hasLower && hasNumber && hasSpecial
+}
+
+func hashPassword(password string) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
 }
